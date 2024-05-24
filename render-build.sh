649a6950b2a57bf8fd93fa6032fedb86c76e77f7
evondev <trananhtuan400@gmail.com>
2024-05-24 21:22:56 +0700

    comment tailwind animate

--- render-build.sh
+++
@@ -1,4 +0,0 @@
-if ! gem list foreman -i --silent; then
-echo “Installing foreman…”
-gem install foreman
-fi
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,6 +39,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
 };
 export default withUt(config);
