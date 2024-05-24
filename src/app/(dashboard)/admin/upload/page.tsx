bd5ca88ab40538fb22bd211ecf9072db5027f1c2
evondev <trananhtuan400@gmail.com>
2024-05-24 13:41:58 +0700

    temp hide upload page

--- src/app/(dashboard)/admin/upload/page.tsx
+++ src/app/(dashboard)/admin/upload/page.tsx
@@ -1,16 +1,22 @@
-import Mux from "@mux/mux-node";
-import MuxUploader from "@mux/mux-uploader-react";
+// import Mux from "@mux/mux-node";
+// import MuxUploader from "@mux/mux-uploader-react";
 
-const mux = new Mux({
-  tokenId: process.env.MUX_TOKEN_ID,
-  tokenSecret: process.env.MUX_TOKEN_SECRET,
-});
+// const mux = new Mux({
+//   tokenId: process.env.MUX_TOKEN_ID,
+//   tokenSecret: process.env.MUX_TOKEN_SECRET,
+// });
 
-export default async function Page() {
-  const directUpload = await mux.video.uploads.create({
-    new_asset_settings: { playback_policy: ["public"] },
-    cors_origin: "*",
-  });
+// export default async function Page() {
+//   const directUpload = await mux.video.uploads.create({
+//     new_asset_settings: { playback_policy: ["public"] },
+//     cors_origin: "*",
+//   });
 
-  return <MuxUploader endpoint={directUpload.url} />;
-}
+//   return <MuxUploader endpoint={directUpload.url} />;
+// }
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
