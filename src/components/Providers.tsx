64d55916ff198bbb5b81d17dfeb2f3fe260572f6
evondev <trananhtuan400@gmail.com>
2024-06-04 00:19:05 +0700

    add alert to users

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -12,6 +12,18 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
         options={{ showSpinner: true }}
         shallowRouting
       />
+      <div className="fixed bottom-0 left-0 right-0 z-[100] p-2 text-sm font-medium bg-red-100 text-red-500 text-center">
+        Nếu tài khoản của bạn không đăng nhập được, vui lòng đăng ký lại nhé.
+        Khóa học trả phí của bạn sẽ tự động được thêm lại. Cần liên hệ mình tại:{" "}
+        <a
+          href="https://fb.com/tuan.trananh.0509"
+          className="font-bold underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook của mình
+        </a>
+      </div>
     </>
   );
 };
