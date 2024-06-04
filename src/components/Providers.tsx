e4623d2b2a421c58705268071df831df8a1f1e91
evondev <trananhtuan400@gmail.com>
2024-06-04 07:14:15 +0700

    chore: update progress bar color in Providers component

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -8,13 +8,13 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
       {children}
       <ProgressBar
         height="3px"
-        color="#2C8FFF"
+        color="#FF695A"
         options={{ showSpinner: true }}
         shallowRouting
       />
-      <div className="fixed bottom-0 left-0 right-0 z-[100] p-2 text-sm font-medium bg-red-100 text-red-500 text-center">
+      <div className="fixed bottom-0 left-0 right-0 z-[100] p-2 text-sm font-medium bg-red-100 text-red-500 text-center hidden lg:block">
         Nếu tài khoản của bạn không đăng nhập được, vui lòng đăng ký lại nhé.
-        Khóa học trả phí của bạn sẽ tự động được thêm lại. Cần liên hệ mình tại:{" "}
+        Khóa học trả phí của bạn sẽ tự động được thêm lại. Liên hệ mình tại:{" "}
         <a
           href="https://fb.com/tuan.trananh.0509"
           className="font-bold underline"
