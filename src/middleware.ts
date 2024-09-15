094528473c39f4994f05fd4da826eb3ba2201fb4
evondev <trananhtuan400@gmail.com>
2024-09-15 09:54:03 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-// const isProtectedRoute = createRouteMatcher(["/admin/(.*)"]);
-export default clerkMiddleware((auth, req) => {
-  // if (isProtectedRoute(req)) auth().protect();
-});
+
+export default clerkMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
