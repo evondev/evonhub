59a1974321cfd875cabe283b2ff01d8de2a88d02
evondev <trananhtuan400@gmail.com>
2024-12-27 20:39:35 +0700

    improve: course content navigation

--- src/components/course/LessonItem.tsx
+++ src/components/course/LessonItem.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { completeLesson } from "@/lib/actions/history.action";
 import { cn } from "@/lib/utils";
-import { formUrlQuery } from "@/utils";
-import { useRouter, useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { IconPlay } from "../icons";
 import { Checkbox } from "../ui/checkbox";
@@ -37,17 +37,9 @@ const LessonItem = ({
     "mb-5 pb-5 border-b border-dashed dark:border-b-slate-500 last:pb-0 last:mb-0 last:border-b-0 flex items-center gap-2 dark:text-text5 text-sm",
     isActive ? "text-primary font-bold dark:text-primary" : "font-medium"
   );
-  const searchParams = useSearchParams();
+  const pathname = usePathname();
   const [isChecked, setIsChecked] = useState(isCompleted);
-  const router = useRouter();
-  const handleChangeLesson = () => {
-    const newUrl = formUrlQuery({
-      params: searchParams?.toString() || "",
-      key: "slug",
-      value: url || "",
-    });
-    router.push(newUrl);
-  };
+
   const handleCompleteLesson = async () => {
     setIsChecked(!isChecked);
     if (!data) return;
@@ -78,9 +70,13 @@ const LessonItem = ({
               "flex-1 text-left",
               isActive ? "pointer-events-none" : "cursor-pointer"
             )}
-            onClick={handleChangeLesson}
           >
-            <div className="line-clamp-1">{title}</div>
+            <Link
+              href={`https://evonhub.dev${pathname}?slug=${url}`}
+              className="block"
+            >
+              <div className="line-clamp-1">{title}</div>
+            </Link>
           </TooltipTrigger>
           <TooltipContent>{title}</TooltipContent>
         </Tooltip>
