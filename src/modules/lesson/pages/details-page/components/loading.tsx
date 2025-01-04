92fb14b2bb58a19f7819fc5370d49b9af7a66fb5
evondev <trananhtuan400@gmail.com>
2025-01-04 17:07:02 +0700

    refactor: enhance loading component styles and structure

--- src/modules/lesson/pages/details-page/components/loading.tsx
+++ src/modules/lesson/pages/details-page/components/loading.tsx
@@ -3,12 +3,12 @@ export interface LoadingProps {}
 export function Loading(_props: LoadingProps) {
   return (
     <div className="w-full mb-8">
-      <div className="w-full aspect-video mb-5">
-        <div className="w-full h-full skeleton"></div>
+      <div className="w-full aspect-video mb-5 min-h-[400px]">
+        <div className="w-full h-full skeleton rounded-lg"></div>
       </div>
       <div className="flex justify-end mb-5 gap-3">
-        <div className="h-12 w-[120px] skeleton"></div>
-        <div className="h-12 w-[120px] skeleton"></div>
+        <div className="h-12 w-[120px] skeleton rounded-lg"></div>
+        <div className="h-12 w-[120px] skeleton rounded-lg"></div>
       </div>
       <div className="h-8 w-full mb-5 skeleton"></div>
     </div>
