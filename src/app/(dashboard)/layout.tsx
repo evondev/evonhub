225b649b8f710b11bac943ab75788f08dcf92caa
evondev <trananhtuan400@gmail.com>
2025-01-04 13:58:14 +0700

    refactor: lesson details part 3

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -37,7 +37,7 @@ export default async function DashboardLayout({
         ></Header>
         <main className="grid grid-cols-1 pt-8 xl:pt-0 xl:w-[calc(100%-300px)] ml-auto lg:min-h-screen relative items-start">
           <Sidebar role={role}></Sidebar>
-          <section className="px-5 lg:px-8 pb-5 lg:pb-10">
+          <section className="px-5 lg:px-8 pb-10">
             {children}
             <Navigation role={role}></Navigation>
           </section>
--- src/modules/lesson/pages/details-page/details-page.layout.tsx
+++ src/modules/lesson/pages/details-page/details-page.layout.tsx
@@ -30,7 +30,7 @@ export function DetailsPageLayout({ children }: DetailsPageLayoutProps) {
   return (
     <div
       className={cn(
-        "flex flex-col lg:grid overflow-hidden lg:overflow-visible h-[calc(100svh-56px)] sm:h-auto grid-cols-1 lg:grid-cols-[minmax(0,2fr),minmax(0,400px)] gap-0 lg:gap-8 items-start transition-all relative -mx-5 -mb-7 lg:mb-0 lg:mx-0",
+        "flex flex-col lg:grid overflow-hidden lg:overflow-visible h-[calc(100svh-172px)] sm:h-auto grid-cols-1 lg:grid-cols-[minmax(0,2fr),minmax(0,400px)] gap-0 lg:gap-8 items-start transition-all relative -mx-5 -mb-7 lg:mb-0 lg:mx-0",
         isExpanded ? "!block" : "lg:grid"
       )}
     >
