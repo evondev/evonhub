bf5a27714a3e48cc539a0a2b7fb40ae491924998
evondev <trananhtuan400@gmail.com>
2025-01-19 23:38:04 +0700

    feat: implement metadata generation for CourseDetailsPage based on course slug

--- src/app/(dashboard)/course/[slug]/page.tsx
+++ src/app/(dashboard)/course/[slug]/page.tsx
@@ -1,6 +1,32 @@
+import { fetchCourseBySlug } from "@/modules/course/actions";
 import { CourseDetailsPage } from "@/modules/course/pages";
+import { Metadata, ResolvingMetadata } from "next";
 
 export interface CourseDetailsPageRootProps {}
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const slug = params.slug;
+
+  const courseDetails = await fetchCourseBySlug(slug);
+
+  return {
+    title: courseDetails?.title,
+    description: courseDetails?.desc,
+    keywords: courseDetails?.seoKeywords,
+    openGraph: {
+      title: courseDetails?.title,
+      description: courseDetails?.desc,
+      images: [courseDetails?.image || "/cover.jpg"],
+    },
+  };
+}
 
 export default function CourseDetailsPageRoot(
   _props: CourseDetailsPageRootProps
